cc9fd1de05a3d19283d099033cacac4f1491db89
Maharaj Teertha Deb <86840917+TeerthaDeb@users.noreply.github.com>
2024-09-09 12:46:08 -0400

    Added Tabs component to DeployedPipelineActions, updated modal content to display Python and JavaScript code snippets, and passed pipeline name as a prop to DeployedPipelineActions in ExecutionDataGrid

--- frontend/src/components/ui/DeployedPipelineActions.jsx
+++ frontend/src/components/ui/DeployedPipelineActions.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { ComboButton, MenuItem, Modal, CodeSnippet } from "@carbon/react";
+import { ComboButton, MenuItem, Modal, CodeSnippet, Tabs, Tab } from "@carbon/react"; // Added Tabs, Tab
 import { Application, TrashCan } from "@carbon/icons-react";
 import { modalContentAtom } from "@/atoms/modalAtom";
 import { useAtom } from "jotai";
@@ -7,12 +7,14 @@ import ClosableModal from "./modal/ClosableModal";
 import { getScheme } from "@/client/anvil";
 
 export const DeployedPipelineActions = ({
+  name,
   uuid,
   hash,
   configuration,
   pipelineData,
 }) => {
   const [modalContent, setModalContent] = useAtom(modalContentAtom);
+
   const generatePostPayload = () => {
     // Generate input structure based on the pipeline graph
     const inputs = {};
@@ -37,21 +39,31 @@ export const DeployedPipelineActions = ({
       }
     });
 
-    const pythonCode = `
+// Python code
+const pythonCode = `
 from zetaforge import Zetaforge
 
-zetaforge = Zetaforge(base_url='${getScheme(configuration.anvil.host)}://${configuration.anvil.host}:${configuration.anvil.port}', token='${configuration.anvil.token}')
+zetaforge = Zetaforge(address='${getScheme(configuration.anvil.host)}://${configuration.anvil.host}:${configuration.anvil.port}', token='${configuration.anvil.token}')
 
-pipeline_uuid = '${uuid}'
-pipeline_hash = '${hash}'
-
-inputs = ${JSON.stringify(inputs, null, 2)}
-
-result = zetaforge.execute_pipeline(pipeline_uuid, pipeline_hash, inputs)
+result = zetaforge.run('${name}:${hash.substring(0, 8)}', input=${JSON.stringify(inputs, null, 2)})
 print('Pipeline execution result:', result)
-    `;
+`;
 
-    // Update modal content
+// JavaScript code
+const jsCode = `
+import Zetaforge from "zetaforge";
+
+const zetaforge = new Zetaforge({
+  address: '${getScheme(configuration.anvil.host)}://${configuration.anvil.host}:${configuration.anvil.port}',
+  token: '${configuration.anvil.token}'
+});
+
+const output = await zetaforge.run('${name}:${hash.substring(0, 8)}', input=${JSON.stringify(inputs, null, 2)});
+console.log(output);
+`;
+
+
+    // Updated modal content to include tabs for Python and JavaScript
     setModalContent({
       ...modalContent,
       content: (
@@ -60,9 +72,24 @@ print('Pipeline execution result:', result)
           passiveModal={true}
           modalClass="custom-modal-size"
         >
-          <CodeSnippet type="multi" feedback="Copied to clipboard">
-            {pythonCode}
-          </CodeSnippet>
+          <Tabs>
+            <Tab
+              label="JavaScript"
+              style={{ color: 'white', backgroundColor: '#333', marginBottom: '10px' , textAlign: 'left'}}
+            >
+              <CodeSnippet type="multi" feedback="Copied to clipboard">
+                {jsCode}
+              </CodeSnippet>
+            </Tab>
+            <Tab
+              label="Python"
+              style={{ color: 'white', backgroundColor: '#333', marginBottom: '10px' , textAlign: 'left' }}
+            >
+              <CodeSnippet type="multi" feedback="Copied to clipboard">
+                {pythonCode}
+              </CodeSnippet>
+            </Tab>
+          </Tabs>
         </ClosableModal>
       ),
     });
